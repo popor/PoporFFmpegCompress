1298af0dddd0bd994d6c0495314ee5d842e7c8ce
wangkq <908891024@qq.com>
2019-07-30 16:43:22 +0800

    带有.a的pod仓库push方法. pod trunk push --use-libraries

--- Example/PoporFFmpegCompress/PoporFFmpegCompressViewController.h
+++ Example/PoporFFmpegCompress/PoporFFmpegCompressViewController.h
@@ -6,6 +6,12 @@
 //  Copyright (c) 2019 popor. All rights reserved.
 //
 
+/*
+ 带有.a的pod仓库push方法.
+ pod trunk push --use-libraries
+ 
+ //*/
+
 @import UIKit;
 
 @interface PoporFFmpegCompressViewController : UIViewController
